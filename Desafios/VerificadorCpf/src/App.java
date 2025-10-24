c9f6e70c70c72abc0954515694aebf1d007508f6
GeovanniMarques <geovannimarques1@gmail.com>
2025-10-24 13:39:15 -0300

    Melhoria de código de desafio

--- Desafios/VerificadorCpf/src/App.java
+++ Desafios/VerificadorCpf/src/App.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
-//java.lang.StringIndexOutOfBoundsException
+// import java.lang.StringIndexOutOfBoundsException
 
 public class App {
     public static void main(String[] args) {
         // Definição de variáveis
-        int d1, d2, d3, d4, d5, d6, d7, d8, d9, dv1, dv2;
-        int soma, resto, verificador;
+        int soma = 0, multiplicador = 10, resto, verificador;
+        int[] digitos = new int[11];
 
         // Realiza limpeza de terminal a cada inicialização do programa
         limparTela();
@@ -19,23 +19,14 @@ public class App {
         //Processamento
 
         //Conversão de String para int
-        d1 = cpf.charAt(0) - 48;
-        d2 = cpf.charAt(1) - 48;
-        d3 = cpf.charAt(2) - 48;
-        d4 = cpf.charAt(3) - 48;
-        d5 = cpf.charAt(4) - 48;
-        d6 = cpf.charAt(5) - 48;
-        d7 = cpf.charAt(6) - 48;
-        d8 = cpf.charAt(7) - 48;
-        d9 = cpf.charAt(8) - 48;
-        dv1 = cpf.charAt(9) - 48;
-        dv2 = cpf.charAt(10) - 48;
+        for (int i = 0; i < 11; i++) {
+            digitos[i] = cpf.charAt(i) - 48;
 
-        // Fechamento de Scanner
-        scanner.close();
-
-        //Cálculo para validação do 1ª dígito verificador
-        soma = (d1 * 10) + (d2 * 9) + (d3 * 8) + (d4 * 7) + (d5 * 6) + (d6 * 5) + (d7 * 4) + (d8 * 3) + (d9 * 2);
+            if(i < 9) {
+                soma += (digitos[i] * multiplicador);
+                multiplicador--;
+            }
+        }
 
         resto = soma % 11;
 
@@ -46,13 +37,18 @@ public class App {
         }
 
         // Comparação de cálculo com 1º dígito verificador
-        if (dv1 != verificador) {
+        if (digitos[9] != verificador) {
             System.out.printf("\nCPF (%s) inválido!", cpf);
             return;
         }
 
         //Cálculo para validação do 2ª dígito verificador
-        soma = (d1 * 11) + (d2 * 10) + (d3 * 9) + (d4 * 8) + (d5 * 7) + (d6 * 6) + (d7 * 5) + (d8 * 4) + (d9 * 3) + (dv1 * 2);
+        multiplicador = 11;
+        
+        for (int i = 0; i < 10; i++) {
+            soma += (digitos[i] * multiplicador);
+            multiplicador--;
+        }
 
         resto = soma % 11;
 
@@ -63,14 +59,14 @@ public class App {
         }
 
         // Comparação de cálculo com 2º dígito verificador
-        if (dv2 != verificador) {
+        if (digitos[10] != verificador) {
             System.out.printf("\nCPF (%s) inválido!", cpf);
             return;
         }
         
         // Saída
         System.out.printf("\nCPF informado (%s) é válido.", cpf);
-        System.out.printf("\n\n1º dígito verificador: %d\n2º dígito verificador 2: %d", dv1, dv2);
+        System.out.printf("\n\n1º dígito verificador: %d\n2º dígito verificador: %d", digitos[9], digitos[10]);
         } catch (Exception e) {
             System.err.println("Ocorreu um erro inesperado.");
         }
